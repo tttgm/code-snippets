e612b54e40d4ef79ed24f235f90fc67ec2bb4b03
Tom M <tmckenzie.nz@gmail.com>
2018-02-11 10:45:26 +1100

    add jupyter commands

---
+++ python-basics/jupyter_magic_commands.py
@@ -0,0 +1,18 @@
+### JUPYTER NOTEBOOK MAGIC COMMANDS ###
+
+# Some useful 'magic' commands for Jupyter Notebooks
+
+%quickref    # display the jupyter Quick Reference Card
+%magic    # display documentation for all available magic commands
+%debug    # enter the interactive debugger at the bottom of the last exception traceback
+%hist    # print command input (and optionally output) history
+%pdb    # automatically enter debugger after any exception
+%paste    # execute pre-formatted python code from clipboard
+%cpaste    # open a special prompt for manually pasting python code
+%reset    # delete all variables / names defined in interactive namespace
+%page OBJECT    # pretty-print the OBJECT and display it through a pager
+%run script.py    # run a python scipt inside the notebook
+%time statement    # report the execution time of a single 'statement'
+%timeit statement    # run a statement multiple times to compute an ensemble average execution time
+%who, %who_ls, %whos    # display variables defined in interactive namespace, with varying levels of information / verbosity
+%xdel variable    # delete a variable and attempt to clear any references to the object in the notebook internals
