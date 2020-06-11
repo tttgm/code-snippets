0189ce18826e66d56c6ff3d0da70ae211af65ca7
Tom M <tmckenzie.nz@gmail.com>
2020-06-11 15:10:44 +1000

    changed savefig example

--- python-plotting/matplotlib_savefig.py
+++ python-plotting/matplotlib_savefig.py
@@ -4,7 +4,12 @@ figure = ax.get_figure()
 figure.savefig(fn, bbox_inches="tight")
 
 ### Save seaborn "small multiples" chart (`FacetGrid`)
-### note: seaborn func call is saved to variable 'g'
+### note: seaborn func call is saved to variable 'g' which is
+### a 'FacetGrid' object
 fn = 'example_FacetGrid_chart.svg'
+g.savefig(fn, bbox_inches = "tight")
+
+### Alternatively, for consistency with above, first get
+### the figure element, then save:
 figure = g.fig
 figure.savefig(fn, bbox_inches = "tight")
